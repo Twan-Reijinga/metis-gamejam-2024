7076d85b2a826cb8c1927009ac7645d6c8ab46ca
Ivi_ <103999115+VelyIvi@users.noreply.github.com>
2024-07-05 10:11:10 +0200

    added controlls

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ var xShift = xResulution/2;
 var yShift = yResulution/2;
 
 
+var controls = new Controls();
+
 function setup() {
     createCanvas(xResulution, yResulution);
 }
 function draw() {
+    if (controls.up){
+        player.y -= 1;
+    }
+    if (controls.down){
+        player.y += 1;
+    }
+    if (controls.left){
+        player.x -= 1;
+    }
+    if (controls.right){
+        player.x += 1;
+    }
     background(220);
     player.Draw(xShift, yShift);
 }
