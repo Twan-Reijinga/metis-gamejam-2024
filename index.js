14a3a08d5acd01cb4d1ce6811e41424fff976658
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2024-07-05 12:17:22 +0200

    load tile sprites

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var xResulution = 1100;
+let imgs = []
+
+function preload() {
+    imgs.push(
+        loadImage('/Assets/Textures/placeholder.png')
+    );
+}
 
 function setup() {
     createCanvas(xResulution, xResulution/5*4);
 }
 function draw() {
     background(220);
+    image(imgs[0], 0, 0, 64, 64);
 }
