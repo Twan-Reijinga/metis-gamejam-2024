02de2b2d12b387ae34e6978e6ef63c64766db276
Ivi_ <103999115+VelyIvi@users.noreply.github.com>
2024-07-05 10:45:06 +0200

    Updated controlls and added timer

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var xResulution = 1100;
+let delta;
 let imgs = []
-
 var xResulution = 6*170;
 var yResulution = xResulution/6*5;
 var player = new Player(0,0);
 var xShift = xResulution/2;
 var yShift = yResulution/2;
+let tileSize = 32;
+
 
 function preload() {
     imgs.push(
@@ -16,9 +17,44 @@ function preload() {
 var controls = new Controls();
 
 function setup() {
-    createCanvas(xResulution, xResulution/5*4);
+    createCanvas(xResulution, yResulution);
+}
+
+var controlTimer = 0;
+var controlTimerMax = 0.2;
+
+function checkControls(delta){
+    controlTimer += delta;
+    if(controlTimer > controlTimerMax){
+        // console.log(controlTimer)
+        if (controls.up){
+            player.y -= 1;
+            controlTimer = 0;
+        }else if (controls.down){
+            player.y += 1;
+            controlTimer = 0;
+        }else if (controls.left){
+            player.x -= 1;
+            controlTimer = 0;
+        }else if (controls.right){
+            player.x += 1;
+            controlTimer = 0;
+        }
+    } else {
+        if (!controls.up && !controls.down && !controls.left && !controls.right){
+            controlTimer = controlTimerMax;
+        }
+    }
+
+
 }
 function draw() {
+    // Update
+    delta = deltaTime*0.001;
+    checkControls(delta);
+
+    // Drawing
     background(220);
     image(imgs[0], 0, 0, 64, 64);
+    player.Draw(xShift, yShift, tileSize);
 }
