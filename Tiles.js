cb6ada26dd6bd2ab5ebf4abb09722ad750cf6cd1
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2024-07-05 12:59:34 +0200

    draw isometric grid (no render order)
    todo render order

--- Tiles.js
+++ Tiles.js
@@ -1,11 +1,25 @@
 class Tiles {
-    drawGrid(width, height) {
-        for(let i = 0; i < width * height; i++) {
-            
+    constructor(width, height, tileSize, tileImgs) {
+        this.width = width;
+        this.height = height;
+        this.tileSize = tileSize;
+        this.tileImgs = tileImgs;
+    }
+
+    drawTiles() {
+        for(let i = 0; i < this.width; i++) {
+            for(let j = 0; j < this.width; j++) {
+                if(j % 2 == 0) {
+                    this.drawTile(i * this.tileSize, (j / 4) * this.tileSize);
+                } else {
+                    this.drawTile((i + 0.5) * this.tileSize, (j / 4) * this.tileSize);
+                }
+            }
         }
     }
 
-    drawTile(x, y) {
-        
+    drawTile(x, y) {    
+        // console.log(this.tileImgs);
+        image(this.tileImgs[0], x, y, this.tileSize, this.tileSize);
     }
 }
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var xResulution = 1100;
 let imgs = []
 
-var xResulution = 6*170;
-var yResulution = xResulution/6*5;
-var player = new Player(0,0);
-var xShift = xResulution/2;
-var yShift = yResulution/2;
+let tiles;
+let player;
+let xResulution = 6*170;
+let yResulution = xResulution/6*5;
+let xShift = xResulution/2;
+let yShift = yResulution/2;
 
 function preload() {
     imgs.push(
@@ -13,12 +13,15 @@ function preload() {
     );
 }
 
-var controls = new Controls();
+let controls = new Controls();
 
 function setup() {
+    tiles = new Tiles(5,5, 64, imgs);
+    player = new Player(0,0);
     createCanvas(xResulution, xResulution/5*4);
+
 }
 function draw() {
     background(220);
-    image(imgs[0], 0, 0, 64, 64);
+    tiles.drawTiles()
 }
